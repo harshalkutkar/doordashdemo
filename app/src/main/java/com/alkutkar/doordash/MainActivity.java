b173096f49de592ce9025a13431d850f4d083e45
harshalkutkar <harshalkutkar@gmail.com>
2017-06-26 09:20:33 -0700

    More tests

--- app/src/main/java/com/alkutkar/doordash/MainActivity.java
+++ app/src/main/java/com/alkutkar/doordash/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
     }
 
 
